c7d883b5709ae419c6c1deec8944f727ba9240bb
Danil Akhtarov <daxartio@gmail.com>
2024-02-22 21:57:19 +0400

    chore(release): version 1.0.0 → 1.1.0

--- kontext/__init__.py
+++ kontext/__init__.py
@@ -1,6 +1,6 @@
 from .log_record import setup_log_record
 from .main import Context, current_context
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 __all__ = ("__version__", "Context", "current_context", "setup_log_record")
